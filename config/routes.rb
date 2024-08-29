01a2973056f05b85603919f8563d697191e144e0
Truong <Truongdavid2162@yahoo.com>
2024-08-29 12:14:47 +0200

    Edit and delete work now

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :lego_sets, only: [ :index, :new, :create, :show ] do
-    resources :bookings, only: [ :index, :new, :create, :show]
+  resources :lego_sets, only: [ :index,:new,:create,:show,:edit,:update, :destroy] do
+    resources :bookings, only: [ :index,:new,:create,:show]
   end
   get "my_bookings", to: "bookings#my_bookings"
   get "my_sets", to: "lego_sets#my_sets"
