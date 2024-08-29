c26c0518e6198de44bcd2886c41b8ec59353b9fe
Truong <Truongdavid2162@yahoo.com>
2024-08-29 16:51:20 +0200

    Status is changed view of my requests is changed as well

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :lego_sets, only: [ :index,:new,:create,:show,:edit,:update, :destroy] do
-    resources :bookings, only: [ :index,:new,:create,:show]
+    resources :bookings, only: [ :index,:new,:create,:show] do
+      member do
+        patch :accept
+        patch :reject
+      end
+    end
   end
   get "my_bookings", to: "bookings#my_bookings"
+  # get "status_change_accepted", to: "bookings#accepted"
   get "my_sets", to: "lego_sets#my_sets"
 end
---
+++ db/migrate/20240829132546_change_column_default_for_bookings.rb
@@ -0,0 +1,5 @@
+class ChangeColumnDefaultForBookings < ActiveRecord::Migration[7.1]
+  def change
+    change_column_default :bookings, :status, 'pending'
+  end
+end
